ab0d7d25933644390e28dd68d871cbe6b4a6c05e
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-04-29 14:20:41 -0500

    updated create meme

--- src/components/MemesContainer.js
+++ src/components/MemesContainer.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import axios from 'axios'
 import Meme from './Meme'
+import update from 'immutability-helper'
+import MemeForm from './MemeForm'
 
 class MemesContainer extends React.Component {
   constructor(props) {
     super(props) 
     this.state = {
-      memes: []
+      memes: [],
+      editingMemeId: null
     }
   }
 
@@ -25,22 +28,27 @@ class MemesContainer extends React.Component {
       'http://localhost:3001/api/memes',
       { meme:
         {
-          top_text: 't',
-          img: 'i', 
-          bottom_text: 'b'
+          top_text: '',
+          img: '', 
+          bottom_text: ''
         }
       }
     )
     .then(response => {
       console.log(response.data)
+      const memes = update(this.state.memes, {
+        $splice: [[0, 0, response.data]]
+      })
+      this.setState({
+        memes: memes,
+        editingMemeId: response.data.id
+      })
     })
     .catch(error => console.log(error))
   }
 
   render() {
     return(
-
-
       <div>
 
         <button className="newMemeButton"
@@ -49,10 +57,14 @@ class MemesContainer extends React.Component {
         </button>
 
         {this.state.memes.map((meme) => {
+          if(this.state.editingMemeId === meme.id) {
+            return(<MemeForm meme={meme} key={meme.id} />)
+          } else {
           return(<Meme meme={meme} key={meme.id} />)
+          }
         })}
       </div>
-      ) 
+    ) 
   }
 }
 
